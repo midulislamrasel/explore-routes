b4f1787b2209b08d17f96a41bbaa7936daa3075e
Midul-islam-Russell <midulraddy121@gmail.com>
2023-01-10 23:43:12 +0600

    Recap

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Contact from './Components/Contact/Contact';
-import Header from './Components/Header/Header';
-import Order from './Components/Order/Order';
-import Main from './layout/Main';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/main',
-      element: <Main></Main>,
-      // children: [{
-        
-      // }]
-    },
-    {
-      path: '/',
-      element : <Header></Header>
-    },
-    {
-      path: '/order',
-      element:<Order></Order>
-    },
-    {
-      path: '/contact',
-      element:<Contact></Contact>
-    }
-  ])
   return (
     <div className="App">
 
-
-      
-      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
