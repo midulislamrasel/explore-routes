b2412758ac49f348d25f62760c23f2ce389d5ca7
Midul-islam-Russell <midulraddy121@gmail.com>
2023-01-11 00:07:26 +0600

    add

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Navbar = () => {
+    return (
+        <div>
+            <h2>This is Navbar Area</h2>
+            <link to='/header'>Header</link>
+            <link to='/contact'>Contact</link>
+            <link to='/order'>Order</link>
+            <link to='/footer'>Footer</link>
+        </div>
+    );
+};
+
+export default Navbar;
